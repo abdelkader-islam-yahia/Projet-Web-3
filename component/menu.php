125c2fada8d0809325b95bd01ed45cc4ff472e39
ayahia <ayahia@gaming.tech>
2023-05-01 16:08:02 +0200

    projet web 3 de l'annee

---
+++ component/menu.php
@@ -0,0 +1,22 @@
+<!--Navigation bar basic-->
+<?php
+    $page = "connexions";
+    require_once "../cfg/config.php";
+?>
+<nav>
+    <div class="nav-wrapper blue">
+        <a href="index.php" class="brand-logo"><img src="img/logo.png" alt="port"></a>
+        <ul class="right hide-on-med-and-down">
+        <div class="modal">
+        
+        </div>
+ 
+        <li><a href='../panel-admin.php'>panel admin</a></li>
+        <li><a href="../queries/project.php">Projets </a></li>
+        
+        
+        </ul>
+    </div>
+</nav>
+
+
